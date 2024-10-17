bea21dd3b934f8db6a1230d545f3e880b1b1cb2d
fatima_ezzahra <fatimaezzahrasafrani@gmail.com>
2024-10-17 00:21:54 +0000

    j ai ajouter la partie du login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,17 +7,34 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { HttpClientModule } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { FormsModule } from '@angular/forms'; // Importer FormsModule ici
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './components/login/login.component'; 
+import { Routes, RouterModule } from '@angular/router';
+import path from 'path';
+import { RhDashComponent } from './components/rh-dash/rh-dash.component';
+import { EmployeDashComponent } from './components/employe-dash/employe-dash.component';
+
+const appRoutes:Routes=[
+  {path:'' , component:LoginComponent},
+  {path:'login' , component:LoginComponent},
+  {path:'rh' , component:RhDashComponent},
+  {path:'employe' , component:EmployeDashComponent}
+]
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ProfileComponent
+    ProfileComponent,
+    LoginComponent,
+    RhDashComponent,
+    EmployeDashComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
     HttpClientModule,
     FormsModule
   ],
